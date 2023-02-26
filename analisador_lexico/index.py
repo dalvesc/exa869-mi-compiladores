8c38003ab6a9358247916fa33c80c6e10bcca30c
dalvesc <dancostafsa@hotmail.com>
2023-02-26 01:26:57 -0300

    analise_para_espaco_depois

--- analisador_lexico/index.py
+++ analisador_lexico/index.py
@@ -29,7 +29,6 @@ def ler_linha_arquivo(arquivo):
     for linha in linhas:
         linha = unicodedata.normalize("NFKD", linha)#transforma \xa0 em espaço
         linhas_formatadas.append(linha)
-
     return linhas_formatadas
 
 #salva quais arquivos estão na pasta de input
@@ -63,11 +62,25 @@ def montar_token(classificacao, lexema, linha):
 def is_palavra_reservada(lexema):
     return lexema in palavras_reservadas
 
+
+def is_operador_aritmetico(lexema):
+    return lexema in operadores_aritmeticos
+
+
+def is_delimitador_comentario(lexema):
+    return lexema in delimitadores_comentarios
+
 #todos os analisadores para passar o lexema
 def analisadores(lexema, linha_encontrada):
-    print(lexema)
+
     if is_palavra_reservada(lexema):
         return tokens.append(montar_token('palavra reservada', lexema, linha_encontrada))
+    elif is_delimitador_comentario(lexema):
+        return tokens.append(montar_token('delimitador de comentario', lexema, linha_encontrada))
+    elif is_operador_aritmetico(lexema):
+        return tokens.append(montar_token('operador aritmetico', lexema, linha_encontrada))
+    else:
+        return tokens_erros.append(montar_token('erro', lexema, linha_encontrada))
     #outros if's para os outros analisadores
 
 #ler linha por linha do arquivo e passar para o analisador
@@ -76,9 +89,22 @@ def analisar_arquivo(linhas):
     for linha in linhas:
         linha_encontrada +=1
         lexema = []
-        for letra in linha:
-            lexema.append(letra)
-            analisadores(''.join(lexema).strip(), linha_encontrada)
+        i = 0
+        while i < len(linha):
+            letra = linha[i]
+            
+            if linha[i] != ' ' and linha[i] != "\t" and linha[i] != "\n":
+                lexema.append(letra)
+
+
+            if linha[++i] == ' '  or linha[++i] == "\t" or linha[++i] == "\n": 
+                if lexema:
+                    analisadores(''.join(lexema).strip(), linha_encontrada)
+                    print(lexema)
+                    lexema = []
+
+
+            i = i + 1
 
 
 if __name__ == "__main__":
